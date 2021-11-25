16e15bd2627fdeab5d26865ead706f0cc262831f
aberatne <aberatne@umich.edu>
2021-11-24 21:48:26 -0500

    Added Spinner and ListView to PreviousWorkoutResults view. Need to get data from database and fill the listview with info.
    ISSUE: In WorkoutSession view, timer doesn't work properly as it skips over the 0 second mark as it counts down.
    
    ISSUE: Getting incorrect results for total workout time, calories burned, and BMI in Results view.

--- app/src/main/java/com/example/fitnessapp/PreviousWorkoutResults.java
+++ app/src/main/java/com/example/fitnessapp/PreviousWorkoutResults.java
@@ -5,20 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class PreviousWorkoutResults extends AppCompatActivity {
 
     Button homeBtn;
-    Spinner sessionSpinner;
-    TextView workoutTime;
-    TextView caloriesBurned;
-    TextView BMITV;
+    Spinner workoutSpinner;
+    ListView userList;
     Button goBackBtn;
 
     Intent v1, v3;
+
+    ArrayAdapter spinnerAdapter;
+    ArrayAdapter listAdapter;
+
+    String[] typeArr = {"Gain Muscle", "Build Endurance", "Lose Weight"};
+
+    ArrayList<Integer> IDList;
+    ArrayList<String> nameList;
+    ArrayList<String> ageList;
+    ArrayList<String> genderList;
+    ArrayList<Integer> weightList;
+    ArrayList<Integer> heightList;
+    ArrayList<Integer> GMTimeList;
+    ArrayList<Integer> BETimeList;
+    ArrayList<Integer> LWTimeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +44,19 @@ public class PreviousWorkoutResults extends AppCompatActivity {
         setContentView(R.layout.activity_previous_workout_results);
 
         homeBtn = findViewById(R.id.home);
-        sessionSpinner = findViewById(R.id.workoutSession);
-        workoutTime = findViewById(R.id.totalWorkoutTime);
-        caloriesBurned = findViewById(R.id.totalCaloriesBurned);
-        BMITV = findViewById(R.id.calculatedBMI);
+        workoutSpinner = findViewById(R.id.workoutType);
+        userList = findViewById(R.id.LV);
         goBackBtn = findViewById(R.id.goBack);
+
+        spinnerAdapter = new ArrayAdapter(PreviousWorkoutResults.this, R.layout.previous_workout_spinner, typeArr);
+
+        workoutSpinner.setAdapter(spinnerAdapter);
+        workoutSpinner.setSelection(0);
+
+        listAdapter = new ArrayAdapter(PreviousWorkoutResults.this, R.layout.previous_workout_list, typeArr);
+
+        userList.setAdapter(listAdapter);
+        userList.setSelection(0);
 
         v1 = new Intent(this, MainActivity.class);
         v3 = new Intent(this, SelectWorkout.class);
